5cbe27dd60dcf88ec9cb39c754dae117bee6c0ad
yusyu26 <yukisakurai929@gmail.com>
2024-11-17 10:12:06 +0900

    Add Heroku host to ALLOWED_HOSTS

--- testTodoproject/settings.py
+++ testTodoproject/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-50iywop=@l0yueg34d_wchdu810w-=5f)$)eb^74a!9r%^3hk('
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['https://testtodo-796b49ca3f40.herokuapp.com/', 'localhost', '127.0.0.1']
 
 
 # Application definition
@@ -122,6 +122,8 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 STATIC_URL = '/static/'
 
